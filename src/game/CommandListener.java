96d75c560a9dee25a3519db0332c3e5699c2ba3f
AnotherProxy <jnsleonard@gmail.com>
2015-10-21 12:06:53 -0400

    CListen Init Comit

---
+++ src/game/CommandListener.java
@@ -0,0 +1,28 @@
+package game;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class CommandListener implements ActionListener {
+
+	private JTextArea out;
+	
+	public CommandListener(JTextArea out){
+		
+		this.out = out;
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JTextField source = (JTextField) e.getSource();
+		String command = source.getText();
+		System.out.println(command);
+		source.setText("");
+		
+	}
+
+}
