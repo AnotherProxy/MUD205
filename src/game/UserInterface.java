9da78b0c134442ae3f8205125db7caa257977882
AnotherProxy <jnsleonard@gmail.com>
2015-10-21 11:17:30 -0400

    2015.10.21

---
+++ src/game/UserInterface.java
@@ -0,0 +1,15 @@
+package game;
+
+import javax.swing.JFrame;
+
+public class UserInterface {
+	
+	public UserInterface(){
+		
+		//Application Window Construction
+		JFrame window = new JFrame();
+		window.setSize(900, 900);
+		
+	}
+
+}
